e89ff763f3987c057a822944b51fd1bf466d72d5
GUSTAVO AYRES LIMA <47052412829@info.local>
2026-09-16 09:26:43 -0300

    :sparkles: adicionando outros elementos

--- manipulacao-dom/script.js
+++ manipulacao-dom/script.js
@@ -42,17 +42,17 @@ elementos.forEach(elemento => {
 // const campo = document.getElementById("campo");
 // const resultado = document.getElementById("resultado");
 
-// // INPUT
-// //evento input -> dispara sempre que digita, em tempo real
-// // campo.addEventListener("input", () => {
-// //     // value -> o que está sendo digitado dentro do campo
-// //     resultado.innerText = campo.value;
-// // })
+// INPUT
+// evento input -> dispara sempre que digita, em tempo real
+// campo.addEventListener("input", () => {
+// value -> o que está sendo digitado dentro do campo
+// resultado.innerText = campo.value;
+// })
 
-// // KEYUP
-// // só dispara quando solta a tecla
+// KEYUP
+// só dispara quando solta a tecla
 // campo.addEventListener("keyup", () => {
-//     // value -> o que está sendo digitado dentro do campo
+// value -> o que está sendo digitado dentro do campo
 //     resultado.innerText = campo.value;
 // })
 
@@ -60,12 +60,12 @@ elementos.forEach(elemento => {
 // const elemento = document.getElementById("troca-cor");
 // const botao = document.getElementById("btn");
 
-// // mouseover -> quando passar o mouse por cima
+// mouseover -> quando passar o mouse por cima
 // elemento.addEventListener("mouseover", () => {
 //     elemento.style.backgroundColor = "#FF0000";
 // })
 
-// // mouseout -> quando o mouse sai
+// mouseout -> quando o mouse sai
 // elemento.addEventListener("mouseout", () => {
 //     // elemento.style.backgroundColor = "#0000FF";
 //     elemento.style.backgroundColor = "";
@@ -77,16 +77,16 @@ elementos.forEach(elemento => {
 //     console.log(`Posição X: ${evento.clientX}, Posição Y: ${evento.clientY}`);
 // })
 
-// // EVENTO DE FORMULÁRIO (submit)
+// EVENTO DE FORMULÁRIO (submit)
 // const form = document.querySelector("form");
 
-// // comportamento padrão do formulário é recarregar a página
+// comportamento padrão do formulário é recarregar a página
 // form.addEventListener("submit", (evento) => {
 
-//     //Impede o comportamento padrão do formulário
+// Impede o comportamento padrão do formulário
 //     evento.preventDefault();
 
-//     // pego a informação digitada no input com ".value"
+// pego a informação digitada no input com ".value"
 //     const nome = document.getElementById("nome").value;
 //     console.log(`Nome: ${nome}`);
 
@@ -99,10 +99,10 @@ elementos.forEach(elemento => {
 
 // const form = document.getElementById("form");
 
-// // estamos adicionando um elemento filho (p) dentro do pai (form)
+// estamos adicionando um elemento filho (p) dentro do pai (form)
 // form.appendChild(novoElemento);
 
-// // criando botão
+// criando botão
 // const botao = document.createElement("button");
 // botao.innerText = "Excluir elemento";
 // form.appendChild(botao);
@@ -112,17 +112,17 @@ elementos.forEach(elemento => {
 
 //     novoElemento.remove();
 
-//     // apagando direto do pai
-//     // form.removeChild(novoElemento);
+// apagando direto do pai
+// form.removeChild(novoElemento);
 // })
 
-// adicionando elementosmentos em uma lista, a partir de um input 
-const input = document.getElementById("input");
-const botao = document.getElementById("botao");
-const lista = document.getElementById("lista");
+// adicionando elementos em uma lista, a partir de um input
+const input = document.getElementById("input"); // input
+const botao = document.getElementById("add"); // button
+const lista = document.getElementById("lista"); // ul
 
 botao.addEventListener("click", () => {
-    const valorDigitado = input.value; // pega o valor dgitado na caixinha/ input
+    const valorDigitado = input.value; // pega o valor digitado na caixinha/input
 
     const li = document.createElement("li");
     li.innerText = valorDigitado;
@@ -131,3 +131,14 @@ botao.addEventListener("click", () => {
 
     input.value = "";
 })
+
+
+
+
+
+
+
+
+
+
+
